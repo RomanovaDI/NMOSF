2cddd0ce6ced511f622fb8e1f9e731fccedbf8a2
Daria Romanova <romanovadi@gmail.com>
2018-03-27 17:39:49 +0300

    It does not work. Avarage velocity of water is 0 and volume of injected fluid is 0.
    Signed-off-by: Daria Romanova <romanovadi@gmail.com>

--- src/lib/utils.c
+++ src/lib/utils.c
@@ -1241,10 +1241,10 @@ int print_oil_production(in *I)
 	double velocity_of_fluid_injection = 0;
 	for (int p = 0; p < 3; p += 2) {
 		velocity_of_fluid_production +=
+			avarage_velocity(I, p, 0, injection_well_coordinates[0] + 1, injection_well_coordinates[1], injection_well_coordinates[2]) * I->dx[1] * I->dx[2] * I->dt +
+			avarage_velocity(I, p, 1, injection_well_coordinates[0], injection_well_coordinates[1] + 1, injection_well_coordinates[2]) * I->dx[0] * I->dx[2] * I->dt -
 			avarage_velocity(I, p, 0, injection_well_coordinates[0] - 1, injection_well_coordinates[1], injection_well_coordinates[2]) * I->dx[1] * I->dx[2] * I->dt +
-			avarage_velocity(I, p, 1, injection_well_coordinates[0], injection_well_coordinates[1] - 1, injection_well_coordinates[2]) * I->dx[0] * I->dx[2] * I->dt -
-			avarage_velocity(I, p, 0, injection_well_coordinates[0] + 1, injection_well_coordinates[1], injection_well_coordinates[2]) * I->dx[1] * I->dx[2] * I->dt -
-			avarage_velocity(I, p, 1, injection_well_coordinates[0], injection_well_coordinates[1] + 1, injection_well_coordinates[2]) * I->dx[0] * I->dx[2] * I->dt;
+			avarage_velocity(I, p, 1, injection_well_coordinates[0], injection_well_coordinates[1] - 1, injection_well_coordinates[2]) * I->dx[0] * I->dx[2] * I->dt;
 	}
 	if (I->time_step == 0) {
 		if ((f = fopen("result/velocity_of_fluid_injection_m.dat","w")) == NULL) {
