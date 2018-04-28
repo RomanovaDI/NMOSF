f43e33912fa3c477bc11831cb06090b39679d9eb
Daria Romanova <romanovadi@gmail.com>
2018-04-28 12:07:38 +0300

    Now ploting temperature, saturation oil and rate of reaction over line from injection we4ll to production work
    Signed-off-by: Daria Romanova <romanovadi@gmail.com>

--- plotdat/plot_T.plt
+++ plotdat/plot_T.plt
@@ -14,3 +14,21 @@ plot	'result/T0.dat' using ($1):($2) with lines ls 1 title "T0", \
 		'result/T3.dat' using ($1):($2) with lines ls 1 title "T3", \
 		'result/T4.dat' using ($1):($2) with lines ls 1 title "T4", \
 		'result/T5.dat' using ($1):($2) with lines ls 1 title "T5"
+set title "Распределение насыщенности нефтью флюида от нагнетающей скважины до добывающей"
+set ylabel "Насыщенность нефтью"
+set output 'images/s_o.png'
+plot	'result/s_o0.dat' using ($1):($2) with lines ls 1 title "s_o0", \
+		'result/s_o1.dat' using ($1):($2) with lines ls 1 title "s_o1", \
+		'result/s_o2.dat' using ($1):($2) with lines ls 1 title "s_o2", \
+		'result/s_o3.dat' using ($1):($2) with lines ls 1 title "s_o3", \
+		'result/s_o4.dat' using ($1):($2) with lines ls 1 title "s_o4", \
+		'result/s_o5.dat' using ($1):($2) with lines ls 1 title "s_o5"
+set title "Распределение скорости химической реакции от нагнетающей скважины до добывающей"
+set ylabel "Скорость химической реакции"
+set output 'images/rate_of_reaction.png'
+plot	'result/rate_of_reaction0.dat' using ($1):($2) with lines ls 1 title "rate_of_reaction0", \
+		'result/rate_of_reaction1.dat' using ($1):($2) with lines ls 1 title "rate_of_reaction1", \
+		'result/rate_of_reaction2.dat' using ($1):($2) with lines ls 1 title "rate_of_reaction2", \
+		'result/rate_of_reaction3.dat' using ($1):($2) with lines ls 1 title "rate_of_reaction3", \
+		'result/rate_of_reaction4.dat' using ($1):($2) with lines ls 1 title "rate_of_reaction4", \
+		'result/rate_of_reaction5.dat' using ($1):($2) with lines ls 1 title "rate_of_reaction5"
--- src/postprocessing.c
+++ src/postprocessing.c
@@ -10,31 +10,31 @@ int main(void)
 	in II;
 	in *I = &II;
 	read_I_termogas(I);
-	int num_res = 43984;
+	int num_res = 46457;
 	int num_plots = 5;
 	for (int i = 0; i <= num_res; i += num_res / num_plots) {
 		char file_name[14 + 20];
-		sprintf(file_name, "result/map%d.vtk", i / (num_res / num_plots));
-		FILE *f = fopen(file_name, "w");
-		sprintf(file_name, "result/s_o%d.dat", i);
+		sprintf(file_name, "result/map%d.vtk", i);
+		FILE *f = fopen(file_name, "r");
+		sprintf(file_name, "result/s_o%d.dat", i / (num_res / num_plots));
 		FILE *f1 = fopen(file_name, "w");
-		char s[100];
+		char s[100], s1[100];
 		int j_pointer = 0;
 		int j;
-		for (j = j_pointer; j < j_pointer + 2; j++)
+		for (j = j_pointer; j < j_pointer + 2; j++) {
 			if (fgets(s, 100, f) == NULL)
 				return 1;
+		}
 		j_pointer = j;
 		double tmp;
-		if (sscanf(s, "oil displacement, time = %lf", &tmp))
-			return 1;
-		fprintf(f1, "#time = %lf\n", tmp);
-		tmp = j_pointer + 2 +
-			I->n_points_multipl * ((int) (I->hight / (I->cellsize / (double) I->kz)) + 1) +
-			I->n_cells * I->kx * I->ky * I->kz * (int) (I->hight / I->cellsize) +
-			I->n_cells * I->kx * I->ky * I->kz * (int) (I->hight / I->cellsize) +
-			I->n_cells_multipl * I->nz;
-		for (j = j_pointer; j < (int) tmp; j++)
+		double time;
+		sscanf(s, "%s %lf", s1, &time);
+		fprintf(f1, "#time = %lf\n", time);
+		int tmp_j = j_pointer + 2 +
+			I->n_points_multipl * ((int) (I->hight / (I->cellsize / (double) I->kz)) + 1) + 1 +
+			I->n_cells * I->kx * I->ky * I->kz * (int) (I->hight / I->cellsize) + 1 +
+			I->n_cells * I->kx * I->ky * I->kz * (int) (I->hight / I->cellsize) + 1 + 1;
+		for (j = j_pointer; j < tmp_j; j++)
 			if (fgets(s, 100, f) == NULL)
 				return 1;
 		j_pointer = j;
@@ -42,38 +42,39 @@ int main(void)
 			if (fgets(s, 100, f) == NULL)
 				return 1;
 		j_pointer = j;
-		for (j = j_pointer; j < j_pointer + (I->nx / 2) * I->ny + I->ny / 2; j++)
+		for (j = j_pointer; j < 2 + j_pointer + (I->nx / 2) * I->ny + I->ny / 2 + 1; j++)
 			if (fgets(s, 100, f) == NULL)
 				return 1;
 		j_pointer = j;
 		sscanf(s, "%lf", &tmp);
 		double step = 0;
-		fprintf(f1, "%lf\t%lf\n", step, tmp);
-		for (int k = 0; k < I->nx / 2; k++) {
-			for (j = j_pointer; j < j_pointer + I->ny + 1; j)
+		fprintf(f1, "%lf %lf\n", step, tmp);
+		for (int k = 0; k < I->nx / 2 - 1; k++) {
+			for (j = j_pointer; j < j_pointer + I->ny + 1; j++)
 				if (fgets(s, 100, f) == NULL)
 					return 1;
 			j_pointer = j;
 			step += I->cellsize * sqrt(2);
 			sscanf(s, "%lf", &tmp);
-			fprintf(f1, "%lf\t%lf\n", step, tmp);
+			fprintf(f1, "%lf %lf\n", step, tmp);
 		}
 		fclose(f1);
 		sprintf(file_name, "result/T%d.dat", i / (num_res / num_plots));
 		f1 = fopen(file_name, "w");
+		fprintf(f1, "#time = %lf\n", time);
 		for (j = j_pointer; j < j_pointer + I->nx * I->ny * I->nz + 2; j++)
 			if (fgets(s, 100, f) == NULL)
 				return 1;
 		j_pointer = j;
-		for (j = j_pointer; j < j_pointer + (I->nx / 2) * I->ny + I->ny / 2; j++)
+		for (j = j_pointer; j < 2 + j_pointer + (I->nx / 2) * I->ny + I->ny / 2 + 1; j++)
 			if (fgets(s, 100, f) == NULL)
 				return 1;
 		j_pointer = j;
 		sscanf(s, "%lf", &tmp);
 		step = 0;
-		fprintf(f1, "%lf\t%lf\n", step, tmp);
-		for (int k = 0; k < I->nx / 2; k++) {
-			for (j = j_pointer; j < j_pointer + I->ny + 1; j)
+		fprintf(f1, "%lf %lf\n", step, tmp);
+		for (int k = 0; k < I->nx / 2 - 1; k++) {
+			for (j = j_pointer; j < j_pointer + I->ny + 1; j++)
 				if (fgets(s, 100, f) == NULL)
 					return 1;
 			j_pointer = j;
@@ -82,21 +83,22 @@ int main(void)
 			fprintf(f1, "%lf\t%lf\n", step, tmp);
 		}
 		fclose(f1);
-		sprintf(file_name, "result/omega%d.dat", i / (num_res / num_plots));
+		sprintf(file_name, "result/rate_of_reaction%d.dat", i / (num_res / num_plots));
 		f1 = fopen(file_name, "w");
+		fprintf(f1, "#time = %lf\n", time);
 		for (j = j_pointer; j < j_pointer + I->nx * I->ny * I->nz + 2; j++)
 			if (fgets(s, 100, f) == NULL)
 				return 1;
 		j_pointer = j;
-		for (j = j_pointer; j < j_pointer + (I->nx / 2) * I->ny + I->ny / 2; j++)
+		for (j = j_pointer; j < 2 + j_pointer + (I->nx / 2) * I->ny + I->ny / 2 + 1; j++)
 			if (fgets(s, 100, f) == NULL)
 				return 1;
 		j_pointer = j;
 		sscanf(s, "%lf", &tmp);
 		step = 0;
 		fprintf(f1, "%lf\t%lf\n", step, tmp);
-		for (int k = 0; k < I->nx / 2; k++) {
-			for (j = j_pointer; j < j_pointer + I->ny + 1; j)
+		for (int k = 0; k < I->nx / 2 - 1; k++) {
+			for (j = j_pointer; j < j_pointer + I->ny + 1; j++)
 				if (fgets(s, 100, f) == NULL)
 					return 1;
 			j_pointer = j;
