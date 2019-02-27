00b4be6a6f4e6cdd2c44ea1692e723917aa90268
Daria Romanova <romanovadi@gmail.com>
2019-02-27 20:07:48 +0300

    Reading constant list
    Signed-off-by: Daria Romanova <romanovadi@gmail.com>

--- NMOSF1/src/MathModel.cpp
+++ NMOSF1/src/MathModel.cpp
@@ -3,15 +3,20 @@
 using namespace std;
 #include "Error.h"
 #include "MathModel.h"
- 
-void variable::setName(char name[50])
+
+variable::variable()
+{
+	memset(varName, 0, TAGLEN * sizeof(char));
+}
+
+void variable::setName(char name[TAGLEN])
 {
 	strcpy(varName, name);
 }
 
 void variable::getName(char *name)
 {
-	strncpy(name, varName, 50);
+	strncpy(name, varName, TAGLEN);
 }
 
 void variable::setVect(int vect)
@@ -24,7 +29,7 @@ int variable::getVect()
 	return varVect;
 }
 
-void equation::setTag(char tag[50])
+void equation::setTag(char tag[TAGLEN])
 {
 	strcpy(eqnTag, tag);
 }
@@ -34,7 +39,7 @@ void equation::getTag(char *tag)
 	strcpy(tag, eqnTag);
 }
 
-void equation::setRecord(char record[500])
+void equation::setRecord(char record[LINELEN])
 {
 	strcpy(eqnRecord, record);
 }
@@ -44,19 +49,103 @@ void equation::getRecord(char *record)
 	strcpy(record, eqnRecord);
 }
 
-void mathModel::readVarList(char fileName[100])
+void equation::setTagComponent(int comp)
+{
+	eqnTagComponent = comp;
+}
+
+int equation::getTagComponent()
+{
+	return eqnTagComponent;
+}
+
+int deleteSpLF(char* str, int len)
+{
+	for (int i = len - 1; i >= 0; i--)
+		if (str[i] == '\n') {
+			str[i] = '\0';
+			len--;
+		}
+	for (int i = 0; i < len; i++)
+		if (str[i] == ' ') {
+			for (int j = i; j < len - 1; j++)
+				str[j] = str[j + 1];
+			str[len - 1] = '\0';
+			len--;
+		}
+}
+
+int whichSimbol(char ch)
+{
+	int tmp = ch;
+	if ((tmp >= '0') && (tmp <= '9'))
+		return 1;
+	if (((tmp >= 'A') && (tmp <= 'Z')) || ((tmp >= 'a') && (tmp <= 'z')))
+		return 2;
+	if (tmp == '_')
+		return 3;
+	if (tmp == '=')
+		return 4;
+	if ((tmp == '*') || (tmp == '/') || (tmp == '+') || (tmp == '-') || (tmp == '^') || (tmp == '?') || tmp == ':')
+		return 5;
+	if (tmp == '.')
+		return 6;
+	if (tmp == '(')
+		return 7;
+	if (tmp == ')')
+		return 8;
+	if (tmp == '[')
+		return 9;
+	if (tmp == ']')
+		return 10;
+	return 0;
+}
+
+void constant::setName(char name[TAGLEN])
+{
+	strcpy(constName, name);
+}
+
+void constant::getName(char *name)
+{
+	strcpy(name, constName);
+}
+
+void constant::setValue(double value)
+{
+	constDimension = 1;
+	constValue[0] = value;
+}
+
+void equation::getRecord(char *record)
+{
+	strcpy(record, eqnRecord);
+}
+
+void equation::setTagComponent(int comp)
+{
+	eqnTagComponent = comp;
+}
+
+int equation::getTagComponent()
+{
+	return eqnTagComponent;
+}
+
+void mathModel::readVarList(char *fileName)
 {
 	DEBUGP(1, FUNC_START);
 	FILE *f = fopen(fileName, "r");
 	if (f == NULL)
 		DEBUGP(0, FILE_OPEN_ERR, fileName);
-	char str[500];
-	char varList_tmp[100][50];
-	int vect[100];
+	char str[LINELEN];
+	memset(str, 0, LINELEN * sizeof(char));
+	char varList_tmp[MAXLIST][TAGLEN];
+	int vect[MAXLIST];
 	varListLen = 0;
 	int flag = 0;
-	char key[50];
-	while (fgets(str, 500, f)) {
+	char key[TAGLEN];
+	while (fgets(str, LINELEN, f)) {
 		if (str[0] == '#')
 			continue;
 		else if (sscanf(str, "%s", key)) {
@@ -65,65 +154,66 @@ void mathModel::readVarList(char fileName[100])
 			else if (! strcmp(key, "FinishVarList"))
 				break;
 			else if (flag == 1) {
-				if ((strlen(key) > 2) && (key[strlen(key) - 2] == '[') && (key[strlen(key) - 1] == ']')) {
-					strncpy(varList_tmp[varListLen], key, strlen(key) - 2);
+				deleteSpLF(str, strlen(str));
+				if ((strlen(str) > 2) && (str[strlen(str) - 2] == '[') && (str[strlen(str) - 1] == ']')) {
+					strncpy(varList_tmp[varListLen], str, strlen(str) - 2);
 					vect[varListLen++] = 1;
 				} else {
-					strcpy(varList_tmp[varListLen], key);
+					strcpy(varList_tmp[varListLen], str);
 					vect[varListLen++] = 0;
 				}
-				if (varListLen == 100)
+				if (varListLen == MAXLIST)
 					DEBUGP(0, FILE_DATA_ERR, "Too many variables. Check \"FinishVarList\" tag.");
 			} else
 				continue;
 		} else
 			DEBUGP(0, FILE_DATA_ERR, "in file", fileName, "in line:", str);
+		memset(str, 0, LINELEN * sizeof(char));
 	}
 	varList = new variable[varListLen];
 	for (int i = 0; i < varListLen; i++) {
 		varList[i].setName(varList_tmp[i]);
 		varList[i].setVect(vect[i]);
 	}
-	calcVarListLenVect();
+	calcEqnListLen();
 	DEBUGP(1, FUNC_FINISH);
 }
 
-void mathModel::calcVarListLenVect()
+void mathModel::calcEqnListLen()
 {
 	DEBUGP(1, FUNC_START);
 	int tmp;
-	//for (int i = 0, varListLenVect = 0; i < varListLen; i++)
 	for (int i = 0; i < varListLen; i++)
 		if (tmp = varList[i].getVect())
 			eqnListLen += dimension;
 		else
 			eqnListLen++;
-	printf("eqnListLen = %d\n", eqnListLen);
 	DEBUGP(1, FUNC_FINISH);
 }
 
-void mathModel::readVarEqn(char fileName[100])
+void mathModel::readVarEqnList(char *fileName)
 {
 	DEBUGP(1, FUNC_START);
 	FILE *f = fopen(fileName, "r");
 	if (f == NULL)
 		DEBUGP(0, FILE_OPEN_ERR, fileName);
-	char str[500];
-	char eqnListTag_tmp[100][50];
-	char eqnList_tmp[100][500];
+	char str[LINELEN];
+	char eqnListTag_tmp[MAXLIST][TAGLEN];
+	char eqnList_tmp[MAXLIST][LINELEN];
+	int eqnListTagComp_tmp[MAXLIST];
 	int listLen_tmp = 0;
 	int flag = 0;
-	char key[50];
-	while (fgets(str, 500, f)) {
+	char key[TAGLEN];
+	while (fgets(str, LINELEN, f)) {
 		if (str[0] == '#')
 			continue;
 		else if (sscanf(str, "%s", key)) {
-			printf("%s\n", key);
-			if (! strcmp(key, "StartVarEqn"))
+			if (! strcmp(key, "StartVarEqnList"))
 				flag++;
-			else if (! strcmp(key, "FinishVarEqn"))
+			else if (! strcmp(key, "FinishVarEqnList"))
 				break;
 			else if (flag == 1) {
+				deleteSpLF(str, strlen(str));
 				char *colon_pointer = strchr(str, ':');
 				if (colon_pointer == NULL)
 					DEBUGP(0, FILE_DATA_ERR, "No expression \"VarName:\".");
@@ -131,12 +221,20 @@ void mathModel::readVarEqn(char fileName[100])
 				char *square_br_pointer = strchr(str, '[');
 				int square_br = square_br_pointer - str;
 				int var_len;
-				if (square_br < colon)
+				if (square_br < colon) {
 					var_len = square_br;
-				else
+					if (square_br != colon - 3)
+						DEBUGP(0, FILE_DATA_ERR, "Error tag of eqation");
+					if (str[colon - 1] != ']')
+						DEBUGP(0, FILE_DATA_ERR, "Error tag of eqation");
+					int comp_tmp = str[square_br + 1] - '0';
+					if ((comp_tmp < 0) || (comp_tmp >= dimension))
+						DEBUGP(0, FILE_DATA_ERR, "Error component", comp_tmp);
+					eqnListTagComp_tmp[listLen_tmp] = comp_tmp;
+				} else
 					var_len = colon;
 				strncpy(eqnListTag_tmp[listLen_tmp], key, var_len);
-				strcpy(eqnList_tmp[listLen_tmp], str);
+				strcpy(eqnList_tmp[listLen_tmp], &str[colon + 1]);
 				listLen_tmp++;
 				if (listLen_tmp > eqnListLen)
 					DEBUGP(0, FILE_DATA_ERR, "Too many equations. Check \"FinishVarList\" tag.");
@@ -149,27 +247,81 @@ void mathModel::readVarEqn(char fileName[100])
 	for (int i = 0; i < eqnListLen; i++) {
 		eqnList[i].setTag(eqnListTag_tmp[i]);
 		eqnList[i].setRecord(eqnList_tmp[i]);
+		eqnList[i].setTagComponent(eqnListTagComp_tmp[i]);
 	}
 	DEBUGP(1, FUNC_FINISH);
 }
 
-void mathModel::readMathModel(char fileName[100], int dim)
+void mathModel::readConstList(char *fileName)
+{
+	DEBUGP(1, FUNC_START);
+	FILE *f = fopen(fileName, "r");
+	if (f == NULL)
+		DEBUGP(0, FILE_OPEN_ERR, fileName);
+	char str[LINELEN];
+	char constList_tmp[MAXLIST][TAGLEN];
+	double constListValue_tmp[MAXLIST];
+	int constListVect_tmp[MAXLIST];
+	constListLen = 0;
+	int flag = 0;
+	char key[TAGLEN];
+	while (fgets(str, LINELEN, f)) {
+		if (str[0] == '#')
+			continue;
+		else if (sscanf(str, "%s", key)) {
+			if (! strcmp(key, "StartConstList"))
+				flag++;
+			else if (! strcmp(key, "FinishConstList"))
+				break;
+			else if (flag == 1) {
+				deleteSpLF(str, strlen(str));
+				char *equality_pointer = strchr(str, '=');
+				if (equality_pointer == NULL)
+					DEBUGP(0, FILE_DATA_ERR, "No expression \"const_name = value\".");
+				int equality = equality_pointer - str;
+				if ((str[equality - 1] == ']') && (str[equality - 3] == '['))
+					d
+				if ((strlen(key) > 2) && (key[strlen(key) - 2] == '[') && (key[strlen(key) - 1] == ']')) {
+					strncpy(varList_tmp[varListLen], key, strlen(key) - 2);
+					vect[varListLen++] = 1;
+				} else {
+					strcpy(varList_tmp[varListLen], key);
+					vect[varListLen++] = 0;
+				}
+				if (varListLen == MAXLIST)
+					DEBUGP(0, FILE_DATA_ERR, "Too many variables. Check \"FinishVarList\" tag.");
+			} else
+				continue;
+		} else
+			DEBUGP(0, FILE_DATA_ERR, "in file", fileName, "in line:", str);
+	}
+	constList = new constant[constListLen];
+	for (int i = 0; i < varListLen; i++) {
+		constList[i].setName(varList_tmp[i]);
+		constList[i].setVect(vect[i]);
+	}
+	calcVarListLenVect();
+	DEBUGP(1, FUNC_FINISH);
+}
+
+void mathModel::readMathModel(char *fileName, int dim)
 {
 	dimension = dim;
 	DEBUGP(1, FUNC_START);
 	readVarList(fileName);
-	readVarEqn(fileName);
+	readVarEqnList(fileName);
 	for (int i = 0; i < varListLen; i++) {
-		char var_tmp[50];
+		char var_tmp[TAGLEN];
 		varList[i].getName(var_tmp);
 		printf("%s[%d]\n", var_tmp, varList[i].getVect() * dimension);
 	}
 	for (int i = 0; i < eqnListLen; i++) {
-		char tag_tmp[50];
-		char rec_tmp[500];
+		char tag_tmp[TAGLEN];
+		char rec_tmp[LINELEN];
 		eqnList[i].getTag(tag_tmp);
 		eqnList[i].getRecord(rec_tmp);
-		printf("%s|||%s\n", tag_tmp, rec_tmp);
+		int comp_tmp = eqnList[i].getTagComponent();
+		printf("%s[%d]: %s\n", tag_tmp, comp_tmp, rec_tmp);
 	}
 	DEBUGP(1, FUNC_FINISH);
 }
--- NMOSF1/src/MathModel.h
+++ NMOSF1/src/MathModel.h
@@ -1,15 +1,22 @@
 #pragma once
 
+#define TAGLEN 50
+#define LINELEN 500
+#define MAXLIST 100
+
+int deleteSpLF(char* str, int len);
+int whichSimbol(char ch);
+
 class variable
 {
 private:
-	char varName[50];
+	char varName[TAGLEN];
 	double *varVal;
-	int varVect;
+	int varVect = 0;
 public:
-	variable() {}
+	variable();
 	~variable() {}
-	void setName(char[50]);
+	void setName(char[TAGLEN]);
 	void getName(char*);
 	void setVect(int);
 	int getVect();
@@ -18,16 +25,36 @@ public:
 class equation
 {
 private:
-	char eqnTag[50];
-	char eqnRecord[500];
+	char eqnTag[TAGLEN];
+	int eqnTagComponent = 0;
+	char eqnRecord[LINELEN];
 public:
 	equation() {}
 	~equation() {}
-	void setTag(char[50]);
+	void setTag(char[TAGLEN]);
 	void getTag(char*);
-	void setRecord(char[500]);
+	void setRecord(char[LINELEN]);
 	void getRecord(char*);
+	void setTagComponent(int);
+	int getTagComponent();
 };
+
+class constant
+{
+private:
+	char constName[TAGLEN];
+	int constDimension = 0;
+	double constValue[3] = {0, 0, 0};
+public:
+	constScalar() {}
+	~constScalar() {}
+	void setName(char[TAGLEN]);
+	void getName(char*);
+	void setValue(double);
+	void setValue(int, double);
+	double getValue();
+	double getValue(int);
+}
 
 class mathModel
 {
@@ -35,13 +62,16 @@ private:
 	int dimension;
 	variable *varList;
 	equation *eqnList;
-	int varListLen;
+	constant *constList;
+	int varListLen = 0;
 	int eqnListLen = 0;
-	void calcVarListLenVect();
-	void readVarList(char[100]);
-	void readVarEqn(char[100]);
+	int constListLen = 0;
+	void calcEqnListLen();
+	void readVarList(char*);
+	void readVarEqnList(char*);
+	void readConstList(char*);
 public:
 	mathModel() {}
-	void readMathModel(char[100], int);
+	void readMathModel(char*, int);
 	~mathModel();
 };
